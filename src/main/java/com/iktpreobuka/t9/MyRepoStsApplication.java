dd29ccfd89d0170e7f834845639b390d3e04dd7f
Vladimir Dimitrieski <dimitrieski89@gmail.com>
2023-02-20 18:25:35 +0100

    init commit

---
+++ src/main/java/com/iktpreobuka/t9/MyRepoStsApplication.java
@@ -0,0 +1,13 @@
+package com.iktpreobuka.t9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRepoStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRepoStsApplication.class, args);
+	}
+
+}
